c134833dbf1e23a65e1baaeedb2b9f3229df86d7
r0ckyang <71221715@qq.com>
2016-07-20 12:35:51 +0800

    Update sqlmapapi_check.py

--- sqlmapapi_check.py
+++ sqlmapapi_check.py
@@ -1,5 +1,7 @@
 # -*- coding: utf_8 -*-
-
+"""
+sqlmap注入过程管理脚本，可控制超时时间及检测到可能有waf无法注入后自动跳出
+"""
 import os
 import sys
 import json
@@ -34,6 +36,19 @@ def task_status(server,taskid):
 	req = requests.get(url)
 	status_check = req.json()['status']
 	return status_check
+
+def task_log(server,taskid):
+	url = server + '/scan/' + taskid + '/log'
+	req = requests.get(url).text
+	scan_json = json.loads(req)['log']
+	flag1 = 0
+	if scan_json:
+		print scan_json[-1]['message']
+		if 'retry' in scan_json[-1]['message']:
+			flag1 = 1
+		else:
+			flag1 = 0
+	return flag1
 
 def task_data(server,taskid):
 	url = server + '/scan/' + taskid + '/data'
@@ -75,12 +90,13 @@ if __name__ == "__main__":
 	server = 'http://127.0.0.1:8775'
 	headers = {'Content-Type':'application/json'}
 	i= 0
+	vuln = []
 
 	for target in targets:
 		try:
-			data = {"url":target,'smart':True}#你可以安需求增加修改sqlmap选项
-			start_time = time.time()
+			data = {"url":target,'batch':True,'randomAgent':True,'tamper':'space2comment','tech':'BT','timeout':15,'level':1}
 			i = i + 1
+			flag = 0
 
 			(new,taskid) = task_new(server)
 			if new:
@@ -94,30 +110,37 @@ if __name__ == "__main__":
 				print "scan can not be started"
 
 			while start:
+				start_time = time.time()
 				status = task_status(server,taskid)
 				if status == 'running':
-					print "scan running"
+					print "scan running:"
 				elif status == 'terminated':
 					print "scan terminated\n"
 					data = task_data(server,taskid)
 					if data:
-						print "+++++ %s is vuln\n" % target
+						print "--------------->>> congratulation! %s is vuln\n" % target
+						vuln.append(target)
 					if not data:
-						print "+++++ the target is not vuln\n"
+						print "--------------->>> the target is not vuln\n"
 					task_delete(server,taskid)
 					break
 				else:
 					print "scan get some error"
 					break
+
 				time.sleep(10)
-				if time.time() - start_time > 3000:
-					print "the scan over time"
+				flag1 = task_log(server,taskid)
+				flag = (flag + 1)*flag1
+
+				if (time.time() - start_time > 300) or (flag == 5):  #此处设置检测超时时间，以及链接超时次数
+					print "there maybe a strong waf or time is over,i will abandon this target."
 					stop = task_stop(server,taskid)
 					if stop:
 						print "scan stoped"
 					if not stop:
 						print "the scan can not be stopped"
 					kill = task_kill(server,taskid)
+					task_delete(server,taskid)
 					if kill:
 						print "scan killed"
 					if not kill:
@@ -125,3 +148,6 @@ if __name__ == "__main__":
 					break
 		except:
 			pass
+
+	for each in vuln:
+		print each + '\n'
